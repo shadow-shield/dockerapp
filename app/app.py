7a8d80ecdf0cdfae02be70b623501a2be2035af5
shadow-shield <shadowgit863@gmail.com>
2025-07-12 15:36:51 -0500

    nuevas vainas

--- app/app.py
+++ app/app.py
@@ -11,12 +11,12 @@ def mainpage():
 
 	key = default_key
 	if 'key' in request.form:
-	    key = request.form['key']
+		key = request.form['key']
 
 	if request.method == 'POST' and request.form['submit'] == 'save':
 		cache.set(key, request.form['cache_value'])
 
-	cache_value = None;
+	cache_value = None
 	if cache.get(key):
 		cache_value = cache.get(key).decode('utf-8')
 
