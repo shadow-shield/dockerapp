c757e76e5feb21968adbf8e8655479ba052dac79
shadow-shield <shadowgit863@gmail.com>
2025-07-12 15:29:52 -0500

    cambios

--- app/app.py
+++ app/app.py
@@ -11,15 +11,15 @@ def mainpage():
 
 	key = default_key
 	if 'key' in request.form:
-	    key = request.form['key']
+		key = request.form['key']
 
 	if request.method == 'POST' and request.form['submit'] == 'save':
 		cache.set(key, request.form['cache_value'])
 
-	cache_value = None;
+	cache_value = None
 	if cache.get(key):
 		cache_value = cache.get(key).decode('utf-8')
-
+  
 	return render_template('index.html', key=key, cache_value=cache_value)
 
 if __name__ == '__main__':
